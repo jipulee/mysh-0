1e9339f62823247d39707ca8c1660cceb9a6f429
jipulee <765149929@qq.com>
2017-10-09 19:05:59 +0900

    fixed strtok method

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,43 @@
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
 #include "utils.h"
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int i = 0,nl = 0; 
+	char *buf,*tokedcmd,*delim;
+
+	nl = strlen(command);
+	buf = (char*)malloc(sizeof(char)*nl);
+	
+	strcpy(buf,command);
+
+	delim = "' ',\n,\t";
+	tokedcmd = strtok(buf,delim);
+	
+	(*argv) = (char**)malloc(sizeof(char*)*100);
+	(*argv)[i]= (char*)malloc(sizeof(char)*100);	
+
+
+	if (tokedcmd!=NULL)
+	{
+		while (tokedcmd !=NULL)
+		{
+			strcpy((*argv)[i],tokedcmd);
+			(*argv)[++i] = (char*)malloc(sizeof(char)*100);
+			tokedcmd = strtok(NULL,delim);
+		}
+	}
+	else {
+		
+		(*argc) = 1;
+		strcpy((*argv)[0],"");
+		return;
+	}
+
+	*argc = i;
+	free(tokedcmd);
+
 }
