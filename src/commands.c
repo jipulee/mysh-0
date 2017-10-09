1208847a1ba1f7f8c8e8e172a31c2e4438c68caf
jipulee <765149929@qq.com>
2017-10-09 19:05:10 +0900

    complete do_cd and cd_pwd method

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
 #include <string.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
-
-  // TODO: Fill it!
-  return 0;
+	if (!validate_cd_argv(argc, argv))
+		return -1;
+	else if (chdir(argv[1]==-1))
+		return 1;
+	else
+		return 0;
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+	if (!validate_pwd_argv(argc, argv))
+		return -1;
 
-  // TODO: Fill it!
+	char *buf;
+	buf=getcwd(NULL,100);
+	printf("directory: %s",buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if ((argc==2) && (strcmp(argv[0],"cd")==0))
+		return 1;
+  
+	else
+		return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	if (argc >1)
+		return 0;
+	
+	else if (strcmp(argv[0], "pwd")!=0)
+		return 0;
+	else 
+		return 1;
 }
